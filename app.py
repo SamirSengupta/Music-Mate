8f2e2cb076e9f464d89f06994f9f64e7c662d1ae
Samir Sengupta <51231316+SamirSengupta@users.noreply.github.com>
2023-07-17 11:23:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,4 +174,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=8080)
